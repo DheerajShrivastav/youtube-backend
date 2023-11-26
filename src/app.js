f156e8d439173dd9dae97eda2eef3201b8e07a43
Dheeraj Shrivastav <dheerajshrivastav08@gmail.com>
2023-11-26 17:37:12 +0530

    Fix import statement and add user routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
-import { express } from 'express'
+import express  from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
-app.use(cors({
-    origin: process.env.CORS-ORIGIN,
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+)
 app.use(express.json({limit: '50Kb'}))
 app.use(express.urlencoded({limit: '16Kb', extends: true}))
 app.use(cookieParser())
+
+// Routes inport
+import userRouter from './routes/user.routes.js'
+
+// Routes declaration
+app.use('/api/v1/users', userRouter)
 
 export default app
