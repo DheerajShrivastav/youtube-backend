36de6ca4dc6ec97141615bb14416b3e47fd383f9
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2023-11-30 18:15:14 +0530

    Fix Cloudinary API Secret in cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,15 +1,23 @@
 import { v2 as cloudinary } from 'cloudinary'
 import fs from 'fs'
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: './.env',
+})
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-const uploadOnCloudinary = async (localFile) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFile) return null
-        cloudinary.uploader.upload(localFilePath, {
+        console.log(localFilePath)
+        if(!localFilePath) return null
+        console.log("this is the local file path", localFilePath)
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: 'auto',
         })
         console.log('file is uploaded on cloudinary')
