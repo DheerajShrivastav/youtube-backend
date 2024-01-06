75e22ba2c43773ecae7b9658e3cc0d78263168ef
Dheeraj Shrivastav <dheerajshrivastav08@gmail.com>
2024-01-07 00:20:35 +0530

    Add login and logout functionality to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { registerUser } from '../controllers/user.controller.js'
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJwt } from '../middlewares/auth.middleware.js'
+import { loginUser, logoutUser } from '../controllers/user.controller.js'
 const router = Router()
 
 router.route('/register').post(
