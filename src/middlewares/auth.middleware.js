491594da8f7174b4a2e6201cba8312a03a33c6e3
Dheeraj Shrivastav <dheerajshrivastav08@gmail.com>
2024-01-07 00:20:47 +0530

    Update auth.middleware.js with new imports and error handling

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,21 +1,30 @@
-import { JsonWebTokenError } from "jsonwebtoken"
-import { asyncHandler } from "../utils/asyncHandler"
-import { User } from "../models/user.model"
+import { asyncHandler } from '../utils/asyncHandler.js'
+import { User } from '../models/user.model.js'
+import { ApiError } from '../utils/ApiError.js'
+import jwt from 'jsonwebtoken'
 
-export const verifyJwt =asyncHandler( async (req, res, next) => {
+export const verifyJwt = asyncHandler(async (req, _, next) => {
   try {
-    const { token } = req?.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    const token =
+      req.cookies?.accessToken ||
+      req.header('Authorization')?.replace('Bearer ', '')
+      // console.log(token)
     if (!token) {
-      throw new ApiError(401, 'Please login to continue')
+      throw new ApiError(401, 'Unauthorized request')
     }
-    const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    const user = await User.findById(decodedToken?._id).select('-password -refreshToken')
-      if (!user) {
-          throw new ApiError(404, 'User not found')
-      }
-    req.user = decoded
-    next()  
+    const decodedToken = await jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id).select(
+      '-password -refreshToken'
+    )
+    if (!user) {
+      throw new ApiError(401, 'Invalid Access Token')
+    }
+    req.user = user
+    next()
   } catch (error) {
-    new ApiError(401, error?.message || 'Please login to continue')
+    throw new ApiError(401, error?.message || 'Please login to continue')
   }
 })
