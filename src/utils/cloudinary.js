2c196fa0348c4451f6222c008348b016a85037e0
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2024-01-26 01:29:13 +0530

    Refactor cloudinary.js and add new functions

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,28 +13,66 @@ cloudinary.config({
 })
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        console.log(localFilePath)
-        if(!localFilePath) return null
-        console.log("this is the local file path", localFilePath)
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: 'auto',
-        })
-        console.log('file is uploaded on cloudinary')
-        return response
-    } catch (error) {
-        fs.unlinkSync(localFilePath)// delete the file from local storage
-        return null
-    }
+  try {
+    console.log(localFilePath)
+    if (!localFilePath) return null
+    console.log('this is the local file path', localFilePath)
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto',
+    })
+    console.log('file is uploaded on cloudinary')
+    return response
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // delete the file from local storage
+    return null
+  }
+}
+function getPublicIdFromCloudinaryUrl(url) {
+  // Remove the file extension
+  let urlWithoutExtension = url.split('.').slice(0, -1).join('.')
+
+  // Get the last part of the path
+  let publicId = urlWithoutExtension.substring(
+    urlWithoutExtension.lastIndexOf('/') + 1
+  )
+
+  return publicId
 }
 const deleteFromCloudinary = async (publicId) => {
-    try {
-        if(!publicId) return null
-        const response = await cloudinary.uploader.destroy(publicId)
-        console.log('file is deleted from cloudinary')
-        return response
-    } catch (error) {
-        return null
-    }
+  try {
+    if (!publicId) return null
+    const response = await cloudinary.uploader.destroy(publicId)
+    console.log('file is deleted from cloudinary')
+    return response
+  } catch (error) {
+    return null
+  }
 }
-export { uploadOnCloudinary, deleteFromCloudinary }
+const getVideoDuration = async (url) => {
+  try {
+    const publicId = getPublicIdFromCloudinaryUrl(url)
+    if (!publicId) return null
+    const response = await new Promise((resolve, reject) => {
+      cloudinary.api.resource(publicId,{resource_type: 'video'}, (error, result) => {
+        if (error) {
+          console.error('Error getting video info:', error)
+          reject(error)
+        } else {
+          console.log(result)
+          resolve(result.duration)
+        }
+      })
+    })
+    return response
+  } catch (error) {
+    console.error('Error in getVideoDuration:', error)
+    return null
+  }
+}
+
+export {
+  uploadOnCloudinary,
+  getPublicIdFromCloudinaryUrl,
+  deleteFromCloudinary,
+  getVideoDuration,
+}
