934090c87ae48e473be9d41c68a6b4c2b81d8ae9
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2024-01-28 00:09:12 +0530

    Add playlist functionality

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import videoRouter from './routes/video.routes.js'
 import commentRouter from './routes/comment.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
 
 // Routes declaration
 app.use('/api/v1/users', userRouter)
@@ -28,4 +29,5 @@ app.use('/api/v1/videos', videoRouter)
 app.use('/api/v1/comments', commentRouter)
 app.use('/api/v1/tweet', tweetRouter)
 app.use('/api/v1/subscription', subscriptionRouter)
+app.use('/api/v1/playlist', playlistRouter)
 export default app
